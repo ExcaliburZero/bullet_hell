782eb60c34221e092920ace293078c8a808ca792
Christopher Wells <cwellsny@nycap.rr.com>
2022-07-04 00:21:43 -0700

    Implement player bullet despawning

---
+++ Assets/Scripts/PlayerBullet.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour
+{
+    public Vector2 velocity;
+    public float speed;
+
+    public List<string> nonCollideableTags = new List<string>(){"Player", "PlayerBullet"};
+    public List<string> destoryCollideableTags = new List<string>(){"ScreenBorder"};
+
+    void FixedUpdate() {
+        transform.Translate(velocity * speed * Time.fixedDeltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision) {
+        Debug.Log("Collided with a: " + collision.tag);
+        if (nonCollideableTags.Contains(collision.tag)) {
+            //Physics.IgnoreCollision(GetComponent<Collider>(), collision.gameObject.GetComponent<Collider>());
+        } else if (destoryCollideableTags.Contains(collision.tag)) {
+            Debug.Log("Destroyed");
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerBulletSpawner.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBulletSpawner : MonoBehaviour
+{
+    public GameObject bulletResource;
+
+    public float bulletSpeed = 5.0f;
+    public float cooldown = 0.2f;
+
+    Vector2 bulletVelocity = new Vector2(0.0f, 1.0f);
+    float timer;
+    bool isShooting;
+
+    void Update() {
+        isShooting = Input.GetButton("Fire1");
+    }
+
+    void FixedUpdate() {
+        timer += Time.fixedDeltaTime;
+
+        if (isShooting && timer >= cooldown) {
+            cooldown = 0.0f;
+
+            ShootBullet();
+        }
+    }
+
+    GameObject ShootBullet() {
+        // TODO: create new transform using only the position
+        GameObject bulletObj = Instantiate(bulletResource, transform.position, transform.rotation);
+
+        PlayerBullet bullet = bulletObj.GetComponent<PlayerBullet>();
+        bullet.velocity = bulletVelocity;
+        bullet.speed = bulletSpeed;
+
+        return bulletObj;
+    }
+}
