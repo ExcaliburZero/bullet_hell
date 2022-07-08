46d2edc536a43737dd539de88c1500401d74d368
Christopher Wells <cwellsny@nycap.rr.com>
2022-07-07 21:48:16 -0700

    Create a more complex bullet pattern

--- Assets/Scripts/BasicEnemyController.cs
+++ Assets/Scripts/BasicEnemyController.cs
@@ -6,10 +6,12 @@ public class BasicEnemyController : MonoBehaviour
 {
     public GameObject bulletResource;
 
-    float shootCooldown = 3.0f;
+    float shootCooldown = 0.2f;
     float bulletSpeed = 1.0f;
 
     float timer = 999999.0f;
+    int offsetIndex;
+    List<float> offsets = new List<float>() { 0.0f, 5.0f, 10.0f, 5.0f, 0.0f, -5.0f, -10.0f, -5.0f };
 
     void FixedUpdate() {
         timer += Time.fixedDeltaTime;
@@ -17,20 +19,53 @@ public class BasicEnemyController : MonoBehaviour
         if (timer >= shootCooldown) {
             timer = 0.0f;
 
-            ShootBullets(8, 180.0f, 360.0f);
+            float offsetDeg = offsets[offsetIndex];
+            Debug.Log("offsetDeg = " + offsetDeg);
+
+            ShootBulletsSimple(8, 180.0f + offsetDeg, 360.0f + offsetDeg);
+
+            offsetIndex = NextOffsetIndex(offsetIndex);
         }
     }
 
-    List<GameObject> ShootBullets(int numBullets, float startAngleDeg, float endAngleDeg) {
+    int NextOffsetIndex(int current) {
+        current += 1;
+
+        if (current == offsets.Count) {
+            current = 0;
+        }
+
+        return current;
+    }
+
+    List<GameObject> ShootBulletsSimple(int numBullets, float startAngleDeg, float endAngleDeg) {
         Debug.Assert(numBullets >= 1);
         Debug.Assert(endAngleDeg >= startAngleDeg);
 
+        float incrementDeg = (endAngleDeg - startAngleDeg) / numBullets;
+
+        return ShootBullets(numBullets, startAngleDeg, endAngleDeg, incrementDeg);
+    }
+
+    List<GameObject> ShootBullets(int numBullets, float startAngleDeg, float endAngleDeg, float incrementDeg) {
+        Debug.Assert(numBullets >= 1);
+
         List<GameObject> bullets = new List<GameObject>();
 
-        float incrementDeg = (endAngleDeg - startAngleDeg) / numBullets;
-        for (float angleDeg = startAngleDeg; angleDeg <= endAngleDeg; angleDeg += incrementDeg) {
+        float angleDeg = startAngleDeg;
+        bool startLessThanEnd = startAngleDeg < endAngleDeg;
+        bool done = false;
+        while (!done) {
             Vector2 direction = Vector2FromAngle(angleDeg);
             bullets.Add(ShootBullet(direction));
+
+            angleDeg += incrementDeg;
+
+            if (startLessThanEnd && angleDeg > endAngleDeg) {
+                done = true;
+            } else if (!startLessThanEnd && angleDeg <= endAngleDeg) {
+                done = true;
+            }
         }
 
         return bullets;
